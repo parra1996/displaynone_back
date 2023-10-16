638eb7691cfcaefa6b8218bffe23f257faaef590
“juanP1996” <parrajp1996@gmail.com>
2023-10-17 00:09:20 +0200

    feat: render2

--- db.js
+++ db.js
@@ -1,8 +1,6 @@
 const { Sequelize, Datatypes } = require('sequelize') 
-const config = require('./config/config.json');
 const dotenv = require('dotenv');
 dotenv.config();
-console.log(process.env.DB_PASSWORD,process.env.DB_USERNAME,process.env.DB_PASSWORD,process.env.DB_HOST,process.env.DB_PORT)
 
 const sequelize = new Sequelize(
     process.env.DB_DATABASE ,
